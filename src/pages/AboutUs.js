dff742955655d9a4a203b3216694eda59c14b81f
GQue - DEVELOPED FRONT END <fcurrao@gmail.com>
2022-09-07 20:49:31 -0300

    add whatsapp
    se agrego el logo de whatsapp

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -5,8 +5,11 @@ const Aboutus = () => {
 
     return (
         <>
-<h4>
-Nuestra experiencia
+<div className="aboutus">
+ <h2 className="textaboutus"> NOSOTROS </h2>
+ </div>
+
+ <p>Nuestra experiencia
 HACEMOS LO QUE AMAMOS DE UNA MANERA DIFERENTE
 Ankara es una empresa que nace de la necesidad de crear una experiencia exclusiva al cliente en una búsqueda de un hogar único,
  exclusivo y prestigioso.
@@ -16,8 +19,7 @@ La experiencia de venta de las casas, es con base a la esencia y personalidad de
 
 Creamos una historia detrás de las propiedades con el acompañamiento de las vivencias de los dueños en los hogares.
 Nuestras visitas a las propiedades consideran una experiencia del entorno, los compradores pueden conocer las áreas
- comerciales, de diversión y esparcimiento para tener una idea completa del ambiente en el que vivirán.
- </h4>
+ comerciales, de diversión y esparcimiento para tener una idea completa del ambiente en el que vivirán.</p>
         </>
     )
 }
