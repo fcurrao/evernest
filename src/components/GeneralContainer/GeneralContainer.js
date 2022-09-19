54405b541c935f90cde764c5b5d66b99fef7441d
GQue - DEVELOPED FRONT END <fcurrao@gmail.com>
2022-09-19 12:37:30 -0300

    renoving

--- src/components/GeneralContainer/GeneralContainer.js
+++ src/components/GeneralContainer/GeneralContainer.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react"
+
+
 
 
 
@@ -9,51 +12,106 @@ const GeneralContainer = () => {
 
     /////////////////////////////////// API ///////////////////////////////////////////
 
-    const url = 
-    'https://api.easybroker.com/v1/properties?page=1&limit=20&search%5Bupdated_after%5D=2020-03-01T23%3A26%3A53.402Z&search%5Bupdated_before%5D=2025-03-01T23%3A26%3A53.402Z&search%5Boperation_type%5D=sale&search%5Bmin_price%5D=500000&search%5Bmax_price%5D=3000000&search%5Bmin_bedrooms%5D=1&search%5Bmin_bathrooms%5D=1&search%5Bmin_parking_spaces%5D=1&search%5Bmin_construction_size%5D=100&search%5Bmax_construction_size%5D=1000&search%5Bmin_lot_size%5D=100&search%5Bmax_lot_size%5D=1000&search%5Bstatuses%5D%5B%5D=published'
 
 
 
     const componentGetMount = async () => {
 
-        const response = await fetch(url)
-        console.group(response)
-        // const api = await fetch(url , {
-        //         method: 'GET',
-        //         headers: new Headers({ 'Content-type': 'application/json' }),
-        //         mode: 'no-cors'
-        //     })
-        //     .then(response => response.json())
-        //     .then(json => console.log(json))
+        const response = await fetch('https://api.easybroker.com/playground#/Properties/get_properties', {
+
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Acces-Control-Allow-Origin': '*',
+                'X-Authorization': 'febj7tgu8e2667ubmz80qetgkmezwy'
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+
+            })
     }
+
+
+
+
+
+    //     const componentGetMount = async () => { 
+
+    //      const response = await fetch('https://api.easybroker.com/playground#/Properties/get_properties'
+    //         mode: 'no-cors',
+    //         headers: {
+    //                     'Content-Type': 'application/json',
+    //                     'Acces-Control-Allow-Origin': '*',
+    //                     'X-Authorization': 'febj7tgu8e2667ubmz80qetgkmezwy'
+    //                 } })
+    //     .then((response) => response.json())
+    //     .then((data)=> {
+    //         console.log(data)
+
+    //     })
+    // }   
+
+
+
+
 
     useEffect(() => {
         componentGetMount()
     }, [])
 
-/////////////////////////////////// API ///////////////////////////////////////////
+
+    /////////////////////////////////// API ///////////////////////////////////////////
 
 
 
-    // const componentGetMount = async () => {
-    //     const respuesta = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    //         
-    //         console.log(respuesta)
-    // }
 
 
 
-    // { componentGetMount() }
+
+    /////////////////////////////////////   URLs //////////////////////////////
+
+    //     API
+    // https://api.easybroker.com/playground#/Properties/get_properties
+
+
+    // help
+    // https://ayuda.easybroker.com/article/330-api-de-easybroker-beta
+
+
+
+
+
+    // API KEY:
+    //  febj7tgu8e2667ubmz80qetgkmezwy
+
+
+
+
+
+
+    ///////////////////////////////// URLs ///////////////////////////////////////////
+
+
+
+
 
 
 
 
 
     return (
-        <><h1>hola</h1></>
+
+        <>
+        {console.log("Cargando el General Container (api) (Base de datos de Ankara) ")}
+        
+        
+        </>
     )
+
 }
 
 
 
-export default  GeneralContainer 
+export default GeneralContainer 
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -124,7 +124,7 @@ const Details = () => {
                     <section className="mmrr62 center lefttotal">
                         <div className="titlfilt"> <h4 className="center ptext3" > </h4> </div>
 
-                        <div className="boxyellow3 ifilters">
+                        <form className="boxyellow3 ifilters">
                             <div className="containerinput">
                                 <input placeholder="Buscar por nombre.." className=" sss form-control inputt sbsb" /><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
@@ -151,7 +151,7 @@ const Details = () => {
                                 <div className="rangess sss form-control inputt" >  <input onChange={() => rangechange()} id="lbrange" type="range" min="000000" max="25000000" step="1000000" />  <div id="lbrangev"> $ 3000000 </div>  </div>
                             </div>
                             <button className="buttonsearch2"><p className="text5">Buscar</p></button>
-                        </div>
+                        </form>
                     </section>
 
                 </section>
